34a09e599c726cadbf6ae515ee2bf425699266da
SteveInGoogle <steve.sanghee@gmail.com>
2019-03-26 15:17:06 -0400

    From 85

--- src/section4/DeadlinedEvent.java
+++ src/section4/DeadlinedEvent.java
@@ -16,4 +16,8 @@ public class DeadlinedEvent extends Event {
 	public String toString() {
 		return title + ", " + deadline.toString();
 	}
+	
+	public MyDate getRepresentativeDate() {
+		return deadline;
+	}	
 }
