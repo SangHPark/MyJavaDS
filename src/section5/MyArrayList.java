3b0642dc7d17d9abd2f5ae92337842db4777d3aa
Steve SangHee <steve.sanghee@gmail.com>
2019-03-30 11:35:18 -0400

    Generic Program my arraylist

--- src/section5/MyArrayList.java
+++ src/section5/MyArrayList.java
@@ -27,6 +27,7 @@ public class MyArrayList<E> {
 		}
 		
 		theData[index] = anEntry;
+		size ++;
 	}
 	
 	private void reallocate() {
@@ -37,7 +38,62 @@ public class MyArrayList<E> {
 	}
 
 	public void add(E anEntry) {
-		
+		add(size, anEntry);
 	}	
+	
+	public int size() {
+		return size;
+	}
+	
+	public int indexOf(E anEntry) {
+		for (int i=0; i<size;i++) {
+			if (theData[i].equals(anEntry)) { //******
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public E get (int index) {
+		if (index < 0 || index >= size) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+		return theData[index];
+	}
+	
+	public E set (int index, E newValue) {
+		if (index < 0 || index >= size) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+		E oldValue = theData[index];
+		theData[index] = newValue;
+		return oldValue;
+	}
+	
+	public E remove(int index) {
+		if (index < 0 || index >= size) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+		
+		E returnValue = theData[index];
+		for (int i = index+1; i< size ; i++) {
+			theData[i-1] = theData[i];
+		}
+		size --;
+		return returnValue;
+	}
+	
+	public static void main(String[] args) {
+		MyArrayList<String> test = new MyArrayList<>();
+		
+		try {
+		test.add(1, "Hello");
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("----------");
+		}
+		
+		System.out.println("NNNNNNNNNN");
+		
+	}
 	
 }
