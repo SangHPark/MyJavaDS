bdf0b9269046694dbe3d7cbbfda02d1905969150
Steve SangHee <steve.sanghee@gmail.com>
2019-03-30 17:07:02 -0400

    singlelinkedlist

--- src/section1/MySingleLinkedList.java
+++ src/section1/MySingleLinkedList.java
@@ -20,21 +20,108 @@ public class MySingleLinkedList <T> {
 		size ++;
 	}
 	
+	public void addAfter(Node<T> before, T item) {
+		Node<T> temp = new Node<T>( item );
+		temp.next = before.next;
+		before.next = temp;
+		size ++;
+	}
+	
+	public T removeFirst() {
+		if (head == null) {
+			return null;
+		}
+		
+			T temp = head.data;
+			head = head.next;
+			size --;
+			return temp;
+	}
+	
+	public T removeAfter(Node <T> before) {
+		if (before == null) {
+			return null;
+		}
+		
+		T temp = before.next.data;
+		before.next = before.next.next;
+		size --;
+		return temp;
+	}
+	
 	public void add(int index, T item) { //insert
+		if (index < 0 || index > size) {
+			return;
+		}
+		if (index == 0) {
+			addFirst(item);
+		} 
+		else {
+			Node<T> q = getNode(index - 1);
+			addAfter(q, item);
+		}
+	}
+	
+	public T remove(int index) {  //delete
+
+		if (index < 0 || index >= size) {
+			return null;
+		}
+		
+		if (index == 0) {
+			return removeFirst();
+		}
+		
+		Node <T> prev = getNode(index - 1);
+		return removeAfter(prev);
 		
 	}
 	
-	public void remove(int index) {  //delete
+
+	
+	public int indexOf(T item) { //search
+		Node <T> p = head;
+		int index = 0;
 		
+		while(p != null) {
+			if (p.data.equals(item)) {
+				return index;
+			}
+			
+			p = p.next;
+			index ++;
+		}
+		
+		return -1;
+	}
+	
+	public Node <T> getNode(int index) {
+		if (index < 0 || index >= size) {
+			return null;
+		}
+		
+		Node <T> p = head;
+		for (int i = 0; i < index; i++) {
+			p = p.next;
+		}
+		
+		return p;
 	}
 	
 	public T get(int index) {
-		return null;
+		if (index < 0 || index >= size) {
+			return null;
+		}
+//		
+//		Node <T> p = head;
+//		for (int i = 0; i < index; i++) {
+//			p = p.next;
+//		}
+//		
+//		return p.data;
+		return getNode(index).data;
 	}
 	
-	public int indexOf(T item) { //search
-		return -1;
-	}
 	
 	public static void main(String[] args) {
 		MySingleLinkedList<String> list = new MySingleLinkedList<>();
